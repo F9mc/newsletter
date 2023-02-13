5162fd21d95633c9545a2e0e1b1c021b7183c940
Paul <paul.cabon@protonmail.com>
2023-02-13 21:56:41 +0100

    fix #2

--- src/mail/mod.rs
+++ src/mail/mod.rs
@@ -2,12 +2,74 @@
 pub mod EmailFunction{
     #[allow(unused_imports)]
     use log::{debug, warn, error, info};
+    use std::{fs, env};
+    use chrono::Utc;
+    use serde::{Serialize, Deserialize};
+    use lettre::transport::smtp::authentication::Credentials;
+    use lettre::{SmtpTransport, Transport};
+    use lettre::message::{header, Message, MultiPart, SinglePart};
     use crate::custom_rss::{Section};
     use crate::mail::EmailBody::{build_mail_msg};
+    
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    struct Receivers{
+        receivers:Vec<String>,
+    }
+
+    impl Receivers{
+        fn emails(&self) -> Vec<String>{
+            let mut emails:Vec<String> = Vec::new();
+            for e in &self.receivers{
+                emails.push(e.clone());
+            }
+            emails
+        }
+    }
+    
+    fn get_receivers() -> Receivers{
+        let file_path = "src/assets/receiver.yaml";
+        let file_data = fs::read_to_string(file_path).unwrap();
+        let receivers:Receivers = serde_yaml::from_str(&file_data).unwrap();
+        debug!("Receivers : {:?}", receivers);
+        receivers
+    }
 
     pub fn send_mail(posts: Vec<Section>){
+        let receivers:Receivers = get_receivers();
         let body:String = build_mail_msg(posts);
         debug!("Body : {:#?}", body);
+
+        let username = match env::var_os("USERNAME") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$USERNAME is not set")
+        };    
+        let password = match env::var_os("PASSWORD") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$PASSWORD is not set")
+        };
+        
+        for receiver in receivers.emails(){
+            debug!("Sending to {:#?}", receiver);
+            let email = Message::builder()
+            .from(username.parse().unwrap())
+            .to("newsletter@pcabon.com".parse().unwrap())
+            .subject(format!("Newsletter {:}", Utc::now().format("%d/%m/%Y")))
+            .singlepart(SinglePart::html(body.clone())).unwrap();
+            
+            let creds = Credentials::new(username.clone(), password.clone());
+    
+            // Open a remote connection to gmail
+            let mailer = SmtpTransport::relay("smtp.gmail.com")
+                .unwrap()
+                .credentials(creds)
+                .build();
+    
+            // Send the email
+            match mailer.send(&email) {
+                Ok(_) => info!("Email sent successfully!"),
+                Err(e) => panic!("Could not send email: {:?}", e),
+            }
+        }
     }
 }
 
@@ -19,7 +81,6 @@ mod EmailBody{
 
     pub fn build_mail_msg(posts:Vec<Section>) -> String {
         let mut html = HtmlPage::new();
-            html.add_header(1, "Newsletter");
 
         for section in posts{
             let mut section_container = Container::new(ContainerType::Article);
