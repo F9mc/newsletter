c594acfae6ca42712bb36e6930d4951870750f16
Paul <paul.cabon@protonmail.com>
2023-02-13 14:30:42 +0100

    fix 3

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 - [Yaml](https://crates.io/crates/serde-yaml)
 - [Serde](https://crates.io/crates/serde)
 - [Rss](https://crates.io/crates/rss)
+- [Lettre](https://crates.io/crates/lettre)
+- [Log](https://crates.io/crates/log)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod feeds;
+use feeds::get_feeds;
+use crate::feeds::Feeds;
+use crate::feeds::Source;
+
+use log::{debug, warn, error, info, Level};
+
+use dotenv::dotenv;
+
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    env_logger::init();
+    info!("Starting");
+    let feeds:Feeds = get_feeds();
+    for categorie in feeds.get_categories(){
+        let categorie_name: String = categorie.get_name();
+        for source in categorie.get_sources(){
+            debug!("{categorie_name} - {:}", source.get_name());
+        }
+    }
 }
